12db6c16a583f6451dc90c81ff71df85d6f77acf
Chineme Arinze <49451533+tnkarize@users.noreply.github.com>
2019-09-26 20:17:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<title>DIV</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+	<!-- START Stylesheets -->
+	
+	<!-- END Stylesheets -->
+
+
+	<!-- START Javascripts -->
+	<script type="text/javascript" src="src/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="src/js/jquery-ui.min.js"></script>
+	<!-- END Stylesheets -->
+
+</head>
+<body>
+	<nav class="navbar navbar-fixed">
+		<ul>
+			<li class="toolPanel" id="create">
+				<a class="toolButton">
+					<i class="ion-ios-plus-outline"></i>
+				</a>
+			</li>
+
+			<li class="toolPanel" id="alter">
+				<a class="toolButton">
+					<i class="ion-ios-toggle-outline"></i>
+				</a>
+			</li>
+
+			<li class="toolPanel" id="search">
+				<a class="toolButton">
+					<i class="ion-ios-search"></i>
+				</a>
+			</li>
+
+			<li class="toolPanel" id="update">
+				<a class="toolButton">
+					<i class="ion-ios-refresh-outline"></i>
+				</a>
+			</li>
+
+			<li class="toolPanel" id="records">
+				<a class="toolButton">
+					<i class="ion-ios-paper-outline"></i>
+				</a>
+			</li>
+
+			<li class="toolPanel" id="settings">
+				<a class="toolButton">
+					<i class="ion-ios-settings"></i>
+				</a>
+			</li>
+
+		</ul>
+
+	</nav>
+	<div class="grid-container-fluid">
+
+
+		<div class="grid-100 view" id="view"></div>
+	</div>
+
+	
+	
+
+
+
+	<!-- START Javascripts -->
+	<script src="js/main.min.js"></script>
+
+
+</body>
+</html>
---
+++ php/test2.php
@@ -0,0 +1,18 @@
+<?php
+	
+	function errorHandler ($message) {
+		die(json_encode(array(
+			'type'     => 'error',
+			'response' => $message
+		)));
+	}
+
+	function successHandler ($message) {
+		die(json_encode(array(
+			'type'     => 'success',
+			'response' => $message
+		)));
+	}
+
+	successHandler('Test 2 Complete');
+?>
